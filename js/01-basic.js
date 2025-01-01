7525c5bf4b1ff839351136d43bf378f29eaba7ba
grypis <grypisnecros@gmail.com>
2025-01-01 18:28:36 +0200

    sad

--- js/01-basic.js
+++ js/01-basic.js
@@ -18,7 +18,7 @@
   city,
   country,
   isStudent,
-  skills = { html, css, js },
+  skills: { html, css, js },
 } = user; */
 
 //! ==========================================================
@@ -134,3 +134,97 @@ console.log(firstNumber);
 console.log(secondNumber); */
 
 //! ==========================================================
+/* const user = {
+  //   username: "Jacob name",
+  age: 25,
+  city: "Dnipro",
+  country: "Ukraine",
+  isStudent: false,
+  skills: {
+    html: true,
+    css: false,
+    js: true,
+  },
+  x: 10,
+};
+
+const {
+  skills: { html, css, js },
+} = user;
+console.log(html); */
+
+//! ==========================================================
+/* const message = {
+  update_id: 936354609,
+  message_id: 1698231,
+  from: {
+    id: 433982686,
+    is_bot: false,
+    first_name: "Volodymyr",
+    username: "pashchenko_v_r",
+    language_code: "uk",
+  },
+  chat: {
+    id: 433982686,
+    first_name: "Volodymyr",
+    username: "pashchenko_v_r",
+    type: "private",
+  },
+  date: 1716567768,
+  text: "Test",
+};
+
+const {
+  from: { first_name, username },
+} = message; */
+
+//! ==========================================================
+/* const messageData = {
+  update_id: 936354609,
+  message: {
+    message_id: 1698231,
+    from: {
+      id: 433982686,
+      is_bot: false,
+      first_name: "Volodymyr",
+      username: "pashchenko_v_r",
+      language_code: "uk",
+    },
+    chat: {
+      id: 433982686,
+      first_name: "Volodymyr",
+      username: "pashchenko_v_r",
+      type: "private",
+    },
+    date: 1716567768,
+    text: "Test",
+  },
+};
+
+const { from: { x, y } = {} } = messageData;
+console.log(x, y); */
+
+//! ==========================================================
+/* const book = {
+  id: 123,
+  author: "Volodka",
+  title: "MyBook",
+  desc: "test",
+};
+
+const { id, ...myBook } = book;
+console.log(myBook); */
+
+//! ==========================================================
+/* const book = {
+  id: 123,
+  author: "Volodka",
+  title: "MyBook",
+  desc: "test",
+  pages: 300,
+  price: 50,
+  year: 2025,
+};
+
+const { id, pages, year, price, ...newBook } = book;
+console.log(newBook); */
